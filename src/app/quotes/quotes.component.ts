6083dd2defa21abe7255fd0ab1acd93b0848c028
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-06-14 20:13:46 +0300

    add fn for displaying author

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -11,7 +11,7 @@ export class QuotesComponent implements OnInit {
   quotes:Quote[] = [
     new Quote (1,'Otemo','“What color is the rainbow, check it next time it shows. Its the way we should be. Altogether in harmony.”','Ritah Marley', new Date(2020,5,14),0,0),
     new Quote (2,'Stacy',"“True love is your soul's recognition of it's counterpoint in another.”",'Max Frisch',new Date(2020,4,14),0,0),
-    new Quote (3,'Lorna','“Don’t trust people whose feelings change with time. Trust people whose feelings remain the same, even when the time changes.”','Bob Marley',new Date(2020,5,14),0,0),
+    new Quote (3,'Lorna','“Don’t trust people whose feelings change with time. Trust people whose feelings remain the same, even when the time changes.”','Bob Marley',new Date(2020,3,12),0,0),
     new Quote (4,'Brian',"“Don't let the behaviour of others destroy your inner peace”",'Rafiki',new Date(2020,5,14),0,0),
     new Quote (5,'Otis','“Most of the important things in the world have been accomplished by people who have kept on trying when there seemed to be no hope at all.”','Dale Carnegie',new Date(2020,5,14),0,0),
     new Quote (6,'Mulamwah','“Whenever you find yourself on the side of the majority, it is time to reform (or pause and reflect).”','Mark Twain',new Date(2020,5,14),0,0),
