94df86a49af0a909c997ae57d1a4e9bab2a46d3f
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-06-14 19:45:43 +0300

    add directive to highlight card

--- src/app/highlight-quote.directive.ts
+++ src/app/highlight-quote.directive.ts
@@ -8,14 +8,14 @@ export class HighlightQuoteDirective {
   constructor(private elem:ElementRef) { 
   }
   @HostListener("click") onClicks(){
-    this.textDeco("green")
+    this.cardDeco("none")
   }
 
   @HostListener("dblclick") onDoubleClicks(){
-    this.textDeco("None")
+    this.cardDeco("rgba(139, 210, 221, 0.301)")
   }
-  private textDeco(action:string){
-    this.elem.nativeElement.style.color=action;
+  private cardDeco(action:string){
+    this.elem.nativeElement.style.background=action;
   }
 
 }
